d0e7294f40fb8da91cf6fc83047ed3d817d6cd1c
malik <malikseo856@gmail.com>
2025-08-30 18:22:59 +0800

    gtag fanskor

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -44,7 +44,14 @@ export default async function LocaleLayout({
           <Script src="./scripts/relic.js" />
         )} */}
         {process.env.NEXT_PUBLIC_NODE_ENV === "production" && (
-          <Script src="./scripts/gTag_head.js" />
+          <Script
+            id="google-tag-manager"
+            strategy="afterInteractive"
+          >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KXMPVSDD');`}</Script>
         )}
         <script
           src="https://analytics.ahrefs.com/analytics.js"
@@ -53,14 +60,16 @@ export default async function LocaleLayout({
         ></script>
       </head>
       <body suppressHydrationWarning={true}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KXMPVSDD"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript>
+        {process.env.NEXT_PUBLIC_NODE_ENV === "production" && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-KXMPVSDD`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+        )}
         <Suspense fallback={<Loading />}>
           <NextAuthProvider>
             <Providers>
